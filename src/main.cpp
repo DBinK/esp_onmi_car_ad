e558a5eb2a15bd34a922ac47a9e11e9cfd372f58
DBinK <DBinKv1@Gmail.com>
2025-04-27 18:39:34 +0800

    feat(main): 增加串口调试 PID 参数功能并优化控制逻辑
    - 添加 UART 类用于接收串口调试指令
    - 实现 PID 参数动态更新功能
    - 优化目标速度的调整逻辑
    - 调整 PID 控制器的输出限制范围
    - 更新变量命名以提高代码可读性

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,27 @@
 
 #include "Motor.hpp"
 #include "HXCEncoder.hpp"
+#include "uart.h"
 
 HXC::Encoder encoder_lf(4, 6);
 Motor motor_lf(1, 2, 0, 1, 1000);
 
 //Define Variables we'll be connecting to
 float ms, out, tg;
-float Kp = 0.26, Ki = 0.0, Kd = 0.000;
+float Kp = 0.19, Ki = 0.19, Kd = 0.0;
 
 QuickPID lfPID(&ms, &out, &tg, Kp, Ki, Kd, QuickPID::Action::direct);
 uint16_t i = 0;
 int8_t reverse = 1;
 
+UART pid_config;   // 串口增强类, 用于接收 vofa+ 调试 PID 参数
+
+float Kp2 = 0.2, Ki2 = 0.08701, Kd2 = 0.000;
+
 void setup() {
     Serial.begin(115200);
     Serial.printf("Start!\n");    
+    pid_config.begin(Serial);  // 初始化串口增强类
     
     encoder_lf.set_filter(100); // 设置脉冲去抖动滤波器的时间常数,单位纳秒
     encoder_lf.setup(50);       // 初始化编码器并设置编码器采样频率,单位Hz 
@@ -29,7 +35,7 @@ void setup() {
 
     lfPID.SetMode(1);
     lfPID.SetOutputLimits(-900, 900);
-    tg = 2000;
+    tg = 1;
 };
 
 void loop() {
@@ -49,15 +55,24 @@ void loop() {
     // Serial.printf("count:%ld, speed:%f\n",count,speed);
 
     i++;
-    if (i > 500) {
-        if (tg > 15000 || tg < 2000){
-            reverse = -reverse;
-        }
+    if (i > 400) {
         tg += 3000 * reverse;
         i = 0;
         // tg = -tg;
+        // lfPID.Reset();
+
+        if (tg > 12000 || tg < 3000){
+            reverse = -reverse;
+        }
     }
 
     Serial.printf("%f,%f,%f,%d\n", ms, out, tg, i);
 
+    // 更新 PID 参数
+    if (pid_config.UpdatePidParams(Kp, Ki, Kd, Kp2, Ki2, Kd2))
+    {
+      lfPID.SetTunings(Kp, Ki, Kd);
+
+      Serial.println("PID参数更新成功");
+    }
 };
